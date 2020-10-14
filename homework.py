bd270722ecb993e2d0a7be928f3b0c4b3fc944b3
Павел <paul-36@mail.ru>
2020-10-15 00:47:58 +0300

    Second

--- homework.py
+++ homework.py
@@ -13,13 +13,17 @@ CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 
 def parse_homework_status(homework):
-    homework_name = homework.get('homework_name')
-    homework_status = homework.get('status')
-    if homework_status == 'rejected':
-        verdict = 'К сожалению в работе нашлись ошибки.'
+    try:
+        homework_name = homework.get('homework_name')
+        homework_status = homework.get('status')
+        if homework_status == 'rejected':
+            verdict = 'К сожалению в работе нашлись ошибки.'
+        else:
+            verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
+    except Exception as e:
+        print(f'Возникла проблема: {e}')
     else:
-        verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
-    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+        return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
 def get_homework_statuses(current_timestamp):
@@ -27,15 +31,21 @@ def get_homework_statuses(current_timestamp):
     headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
     from_date = current_timestamp
     url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
-    homework_statuses = requests.get(url, headers=headers, params={
-        'from_date': from_date,
-    }).json()
 
-    return homework_statuses
+    try:
+        homework_statuses = requests.get(url, headers=headers, params={
+            'from_date': from_date,
+        }).json()
+    except Exception as e:
+        print(f'Возникла проблема: {e}')
+    else:
+        return homework_statuses
+
+
+bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
 
 def send_message(message):
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
     return bot.send_message(chat_id=CHAT_ID, text=message)
 
 
@@ -48,7 +58,9 @@ def main():
             if new_homework.get('homeworks'):
                 send_message(parse_homework_status(new_homework.get
                                                    ('homeworks')[0]))
-            current_timestamp = new_homework.get('current_date')
+            check_time = new_homework.get('current_date')
+            if check_time is not None:
+                current_timestamp = check_time
             time.sleep(300)
 
         except Exception as e:
